0b4e3b8b2b9f057e2cad4e73be0aa1909c387d45
AhmadAlbara <ahmadalbara64@gmail.com>
2024-05-25 23:56:50 +0700

    add search genre features

--- src/app/genres/page.jsx
+++ src/app/genres/page.jsx
@@ -1,31 +1,57 @@
+"use client";
+import { useEffect, useState } from "react";
 import { fetchDataAnime } from "@/libs/api-libs";
 import Link from "next/link";
+import Loading from "../loading";
 
-const Page = async () => {
-  const api = await fetchDataAnime("genres/anime");
+const Page = () => {
+  const [genres, setGenres] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    const fetchGenres = async () => {
+      setIsLoading(true);
+      try {
+        const response = await fetchDataAnime("genres/anime");
+        setGenres(response.data);
+      } catch (error) {
+        console.error("Error fetching genres:", error);
+      }
+      setIsLoading(false);
+    };
+
+    fetchGenres();
+  }, []);
+
   return (
     <div className="p-4">
-      <ul className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
-        {api.data.map((genre) => (
-          <li key={genre.name}>
-            <Link
-              href={{
-                pathname: `genres/animes/`,
-                query: {
-                  genreName: genre.name,
-                  genreId: genre.mal_id,
-                },
-              }}
-              passHref
-            >
-              <div className="flex items-center justify-between p-4 transition-transform border rounded-lg cursor-pointer border-color-secondary dark:border-color-primary hover:scale-105 hover:bg-gray-300 dark:hover:bg-gray-800">
-                <span className="text-lg font-medium ">{genre.name}</span>
-                <span className="text-sm ">{genre.count} anime</span>
-              </div>
-            </Link>
-          </li>
-        ))}
-      </ul>
+      {isLoading ? (
+        <Loading />
+      ) : (
+        <>
+          <ul className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
+            {genres.map((genre) => (
+              <li key={genre.name}>
+                <Link
+                  href={{
+                    pathname: `genres/animes/`,
+                    query: {
+                      genreName: genre.name,
+                      genreId: genre.mal_id,
+                    },
+                  }}
+                  passHref
+                >
+                  <div className="flex items-center justify-between p-4 transition-transform border rounded-lg cursor-pointer border-color-secondary dark:border-color-primary hover:scale-105 hover:bg-gray-300 dark:hover:bg-gray-800">
+                    <span className="text-lg font-medium ">{genre.name}</span>
+                    <span className="text-sm ">{genre.count} anime</span>
+                  </div>
+                </Link>
+              </li>
+            ))}
+          </ul>
+        </>
+      )}
     </div>
   );
 };
